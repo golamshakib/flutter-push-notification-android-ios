7a6b3bb505af12f6f226834306d4978455b54b9b
golamshakib <golamshakib.h@gmail.com>
2025-07-30 20:43:23 +0600

    added initialize the local notification plugin for andoir and iOS

--- lib/core/services/local_notification_service.dart
+++ lib/core/services/local_notification_service.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class LocalNotificationService {
@@ -35,8 +37,37 @@ class LocalNotificationService {
   );
 
   // Flag to track initialization status
-  bool _isFlutterLocalNotificaionInitialized = false;
+  final bool _isFlutterLocalNotificationInitialized = false;
 
   // Counter for generating unique notification IDs
-  int _notificationIdCounter = 0;
+  final int _notificationIdCounter = 0;
+
+  /// Initializes the local notifications plugin for Android and iOS.
+  Future<void> init () async {
+
+    // Check if already initialized to prevent redundant setup
+    if(_isFlutterLocalNotificationInitialized){
+      return;
+    }
+
+    // Create plugin instance
+    _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+    // Combine platform-specific settings
+    final initializationSettings = InitializationSettings(
+      android: _androidInitializationSettings,
+      iOS: _iosInitializationSettings,
+    );
+
+    // Initialize plugin with settings and callback for notification taps
+    await _flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) {
+      // Handle notification tap in foreground
+        log('Foreground notification has been tapped: ${response.payload}');
+      }
+    );
+
+    // Create Android notification channel
+  }
 }
